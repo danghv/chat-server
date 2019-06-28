61888d94cfbcfcde21cbdfcb7772bc42d0f2a05a
Dang Ha Van (danghv1681) <ha.van.dang@sun-asterisk.com>
2019-06-28 16:26:05 +0700

    init

---
+++ public/precache-manifest.e332ad24c6d6dd70363770d3dde6053c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8eb8e82f26050a08283edf3713f73f86",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea52962cebdcf22a3392",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "5316bbe18d35d9c1505e",
+    "url": "/static/js/2.4263f0ca.chunk.js"
+  },
+  {
+    "revision": "ea52962cebdcf22a3392",
+    "url": "/static/js/main.6663e608.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
